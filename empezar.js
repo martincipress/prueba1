25a4333d498fcca190db3086bfa96691693dfc42
martincipress <90207409+martincipress@users.noreply.github.com>
2021-10-14 20:43:59 -0300

    primera ejercicio

---
+++ empezar.js
@@ -0,0 +1,25 @@
+//la intruccion conole.log muestra 
+//un dato en pantalla
+
+
+console.log(123) //numero number
+console.log(12.5)// decimal number
+console.log("un numero 123") //string con ""
+console.log('otro numero 123')//string con ''
+console.log(Date) //fecha en formato utc timestamp
+
+//variables
+const name = "Juana"  //una constante
+let last_name ="Garcia" // variable let
+var age = '20' // variable var
+
+//para concatenar se usa + y 
+
+var nombre = "Martin" // crear variable nombre
+var apellido = 'Espinilla' // crear variable apellido
+var edad = 42 //creamos var edad
+
+console.log(`${nombre} ${apellido}  tiene ${edad} años`) //mostramos en pantalla datos concatebados
+
+var edad = 42 + 1 //sumamos 1 a la variable edad
+console.log(`${nombre} ${apellido}  tiene ${edad} años`) //volvemos a mostrar en pantalla
